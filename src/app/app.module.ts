9f9ec791e7c55faa7778f166c354b2aef75a7c95
Rakesh Kavitake <rakesh.kavitake@gmail.com>
2021-03-12 13:00:57 +0530

    search page alignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {HeadroomModule} from '@ctrl/ngx-headroom';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { PropertySearchComponent } from './property-search/property-search.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertySearchComponent
   ],
   imports: [
     BrowserModule,
     HeadroomModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
